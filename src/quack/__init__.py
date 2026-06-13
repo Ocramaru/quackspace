f2520bcc55a1224e55d08dbbfcfa1e5169aa0c51
Ocramaru <marcocassar@mdcusa.net>
2026-06-13 00:01:44 -0500

    Derive version from git tags instead of hardcoding

--- src/quack/__init__.py
+++ src/quack/__init__.py
@@ -1,3 +1,8 @@
 """quack, a knowledge layer over your local work that helps LLMs navigate it."""
 
-__version__ = "0.1.0"
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("quackspace")
+except PackageNotFoundError:
+    __version__ = "0.0.0"
